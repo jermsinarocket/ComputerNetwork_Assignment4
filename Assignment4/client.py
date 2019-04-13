bfd22ef3ae74959e4b057eaff5191a76c62cdee3
Tan Wei Xuan <jermsinarocket@gmail.com>
2019-04-13 18:13:38 +0900

    Final

--- Assignment4/client.py
+++ Assignment4/client.py
@@ -4,6 +4,13 @@ import socket
 from threading import Thread
 import pyaudio
 from ctypes import *
+import tkinter
+from PIL import Image
+from PIL import ImageTk
+import threading
+import sys
+import cv2
+import io
 
 # Audio quality
 CHUNK = 8192
@@ -13,6 +20,13 @@ RATE = 44100
 WIDTH = 2
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
+#Capture for Webcam
+cap = cv2.VideoCapture(0)
+
+#Work around in case webcam is not opened
+if cap.read() == False:
+	cap.open()
+
 # Prevents the ALSA debug information from spamming stdout
 def py_error_handler(filename, line, function, err, fmt):
 	pass
@@ -20,8 +34,6 @@ def py_error_handler(filename, line, function, err, fmt):
 
 # PyAudio configurations
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so.2')
-asound.snd_lib_error_set_handler(c_error_handler)
 
 p = pyaudio.PyAudio()
 
@@ -47,7 +59,7 @@ def receive_text():
 	while True:
 		try:
 			msg = client_socket_text.recv(BUFSIZ).decode("utf8")
-			print(msg)
+			msg_list.insert(tkinter.END,msg)
 		except OSError:
 			break
 
@@ -61,17 +73,63 @@ def receive_voice():
 		except OSError:
 			break
 
+# For Receiving Video
+def receive_video():
+	global panel
+	while not stop_video.is_set():
+		try:
+			ttlrec = 0
+			metarec = 0
+
+			msgArray = []
+			metaArray = []
+
+			while metarec < 8:
+				chunk = client_socket_video.recv(8 - metarec).decode("utf8")
+				if chunk == '':
+					raise RuntimeError("Socket Connection has been broken")
+				metaArray.append(chunk)
+				metarec += len(chunk)
+
+			lengthstr = ''.join(metaArray)
+			length = int(lengthstr)
+
+			while ttlrec < length:
+				chunk = client_socket_video.recv(length - ttlrec)
+				if chunk == '':
+					raise RuntimeError("Socket Connection has been broken")
+				msgArray.append(chunk)
+				ttlrec += len(chunk)
+
+			frame = b''.join(msgArray)
+			pil_bytes = io.BytesIO(frame)
+			pil_img = Image.open(pil_bytes)
+			img = ImageTk.PhotoImage(pil_img)
+
+			if panel is None:
+				panel = tkinter.Label(video_frame, image =img)
+				panel.image = img
+				panel.pack(side=tkinter.TOP, expand = True)
+			else:
+				panel.configure(image = img)
+				panel.image = img
+
+		except OSError:
+			break
 
 # Send Text to the Server
-def send_text():
-	while True:
-		msg = input()
-		print('\033[A\033[A')
-		client_socket_text.send(bytes(msg, "utf8"))
-		if msg == "{quit}":
-			client_socket_text.close()
-			client_socket_voice.close()
-			quit()
+def send_text(a=1):
+	msg = my_msg.get()
+	client_socket_text.send(bytes(msg,"utf8"))
+
+	if msg == "{quit}":
+		client_socket_text.close()
+		client_socket_voice.close()
+		top.destroy()
+		top.quit()
+		stop_video.set()
+		quit()
+	my_msg.set("")
 
 
 # Send voice to the server
@@ -84,13 +142,119 @@ def send_voice():
 			break
 
 
-HOST = input("Enter the Host: ") # Default 127.0.0.1
-PORT_TEXT = 2222   # TEXT port of server to connect to
-PORT_VOICE = 55555 # VOICE port of server to connect to
+#Send Video to the server
+def send_video():
+	global cap
+
+	while not stop_video.is_set():
+		ret_val, img = cap.read()
+		img = cv2.resize(img, (360,360))
+
+		cv2_im = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		pil_im = Image.fromarray(cv2_im)
+		b_io = io.BytesIO()
+		pil_im.save(b_io,'jpeg')
+		framestr = b_io.getvalue()
+
+		ttlsent = 0
+		metasent = 0
+		length = len(framestr)
+		lengthstr = str(length).zfill(8)
+
+		while metasent < 8:
+			sent = client_socket_video.send(lengthstr[metasent::].encode("utf8"))
+			if sent == 0:
+				raise RuntimeError("Socket Connection has broken")
+			metasent += sent
+
+		while ttlsent < length:
+			sent = client_socket_video.send(framestr[ttlsent::])
+			if sent == 0:
+				raise RuntimeError("Socket Connection has broken")
+			ttlsent += sent
+
+# Handler when windows is closed
+def on_closing(event=None):
+	my_msg.set("{quit}")
+	send_text(1)
+
+
+#Cam Capture to Screen
+def show_my_video():
+	global cap
+	panel = None
+
+	while not stop_video.is_set():
+		ret_val, img = cap.read()
+
+		img = cv2.resize(img, (120, 120))
+
+		image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+		image = Image.fromarray(image)
+		image = ImageTk.PhotoImage(image)
+
+		if panel is None:
+			panel = tkinter.Label(image=image)
+			panel.image = image
+			panel.place(height=110,width=110,x=256,y=5)
+		else:
+			panel.configure(image=image)
+			panel.image = image
+
+#TKinter Settings
+top = tkinter.Tk()
+top.title("Internet Voice,Video and Text Chatting Programme")
+
+video_frame = tkinter.Frame(top)
+video_frame.pack(side=tkinter.TOP)
+
+messages_frame = tkinter.Frame(top)
+messages_frame.pack(side=tkinter.TOP)
+
+button_frame = tkinter.Frame(top)
+button_frame.pack(side=tkinter.TOP)
+
+my_msg = tkinter.StringVar()
+my_msg.set("Enter your Name")
+
+scrllBar = tkinter.Scrollbar(messages_frame)
+
+entry_field = tkinter.Entry(button_frame, textvariable=my_msg)
+entry_field.bind("<Return>", send_text)
+entry_field.pack(side=tkinter.LEFT, padx = 10)
+
+send_button = tkinter.Button(button_frame, text="Send", command = send_text)
+send_button.pack(side=tkinter.LEFT, padx = 10)
+
+send_button = tkinter.Button(button_frame, text="test", command = send_text)
+send_button.pack(side=tkinter.LEFT, padx = 10)
+
+msg_list = tkinter.Listbox(messages_frame, height = 15, width =50, yscrollcommand = scrllBar.set)
+scrllBar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.TOP, fill=tkinter.BOTH)
+
+top.protocol("WM_DELETE_WINDOW, on_closing")
+
+try:
+	HOST = sys.argv[1]
+except IndexError:
+	print("Usage: ./client [Host]")
+	quit()
+
+PORT_TEXT = 2222   # Text port of server being connected to
+PORT_VOICE = 55555 # Voice port of the server being connected to
+PORT_VIDEO = 1026  # Video port of the server being connected to
 
 BUFSIZ = 1024
+
 ADDR_TEXT = (HOST, PORT_TEXT)
 ADDR_VOICE = (HOST, PORT_VOICE)
+ADDR_VIDEO = (HOST, PORT_VIDEO)
+
+#For Video
+panel = None
+
+stop_video = threading.Event()
 
 """
 Attempts to connect to the server Text Port
@@ -101,18 +265,24 @@ client_socket_text.connect(ADDR_TEXT)
 """
 Attempts to connect to the server Voice Port
 """
-
 client_socket_voice = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket_voice.connect(ADDR_VOICE)
 
-receive_text_thread = Thread(target=receive_text)
-receive_text_thread.start()
+"""
+Attempts to connect to the server Video Port
+"""
+client_socket_video = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket_video.connect(ADDR_VIDEO)
 
-send_text_thread = Thread(target=send_text)
-send_text_thread.start()
 
-receive_voice_thread = Thread(target=receive_voice)
-receive_voice_thread.start()
+receive_text_thread = Thread(target=receive_text).start()
 
-#Keep sending voice
-send_voice()
+receive_voice_thread = Thread(target=receive_voice).start()
+send_voice_thread = Thread(target=send_voice).start()
+
+send_video_thread = Thread(target=send_video).start()
+receive_video_thread = Thread(target=receive_video).start()
+
+show_myvideo_thread = Thread(target=show_my_video).start()
+
+tkinter.mainloop()
--- Assignment4/server.py
+++ Assignment4/server.py
@@ -15,7 +15,6 @@ def accept_text():
 		client, client_address = SERVER_TEXT.accept()
 		print("%s:%s has connected to the Server" % client_address)
 		client.send(bytes("Enter your name: ", "utf8"))
-		addresses_text[client] = client_address
 		Thread(target=handle_client_text, args=(client,)).start()
 
 
@@ -26,9 +25,18 @@ def accept_voice():
 	"""
 	while True:
 		client, client_address = SERVER_VOICE.accept()
-		addresses_voice[client] = client_address
 		Thread(target=handle_client_voice, args=(client,)).start()
 		print("Voice has been enabled" )
+
+# For handling client's connection to VIDEO PORT, assign a thread tio handle each client connection
+def accept_video():
+    """
+    Accept clients' connection to VIDEO port.
+    """
+    while True:
+        client, client_address = SERVER_VIDEO.accept()
+        Thread(target=handle_client_video, args=(client,)).start()
+        print("Video has been enabled")
 
 def handle_client_text(client):
 	"""
@@ -65,46 +73,82 @@ def handle_client_voice(client):
 	while client in clients_voice:
 		try:
 			data = client.recv(BUFSIZ)
-			broadcast(data, dtype='voice')
+			broadcast(data, dtype='voice', sd = client)
 		except Exception as _:
 			client.close()
 			del clients_voice[client]
 
+def handle_client_video(client):
+	"""
+	Handles Video from the given socket.
+ 	"""
+	# Receive Video
+	clients_video[client] = 1
+	while client in clients_video:
+            ttlrec = 0
+            metarec = 0
+            msgArray = []
+            metaArray = []
+
+            while metarec < 8:
+                chunk = client.recv(8 - metarec)
+                metaArray.append(chunk)
+                metarec += len(chunk)
+
+            length = int(chunk.decode("utf8"))
+
+            while ttlrec < length:
+                chunk = client.recv(length-ttlrec)
+                if chunk == '' :
+                    raise RuntimeError("Socket Connection has broken")
+                msgArray.append(chunk)
+                ttlrec += len(chunk)
+
+            broadcast(b''.join(metaArray + msgArray),dtype="video", sd=client)
 
 # Delete a text socket from client list
 def erase_client(client):
-	"""
+    """
     Removes Socket from Client List.
 	"""
-	del clients_text[client]
-	del addresses_text[client]
+    del clients_text[client]
+    del clients_voice[client]
+    del clients_video[client]
 
 
 # Broadcast a message to every client, depends on whether it is voice or text
-def broadcast(msg, prefix="", dtype='text',):
-	"""
+def broadcast(msg, prefix="", dtype='text', sd = None):
+    """
     Broadcast message to clients, depending on data type.
 	"""
-	# Check if data is voice or text
-	if dtype == 'text':
-		for sock in clients_text:
-			sock.send(bytes(prefix, "utf8") + msg)
-	else:
-		for sock in clients_voice:
-			sock.send(msg)
+    # Check if data is voice or text
+    if dtype == 'text':
+        for sock in clients_text:
+            sock.send(bytes(prefix, "utf8") + msg)
+    elif dtype == 'voice':
+        for sock in clients_voice:
+            if sd != sock:
+                sock.send(msg)
+    elif dtype == 'video':
+        for sock in clients_video:
+            if sd != sock:
+                sock.send(msg)
 
 
 clients_text = {}
-addresses_text = {}
 clients_voice = {}
-addresses_voice = {}
+clients_video = {}
 
 HOST = '' # current machine's hostname - 127.0.0.1  (Change Accordingly)
 PORT_TEXT = 2222 # Text Port of Server
 PORT_VOICE = 55555 # Voice Port of Server
+PORT_VIDEO = 1026 # Video Port of Server
+
 BUFSIZ = 1024 # buffer size for messages and Voices
+
 ADDR_TEXT = (HOST, PORT_TEXT) # Bindings for text socket
-ADDR_VOICE = (HOST, PORT_VOICE) # Bindings for voice sockfet
+ADDR_VOICE = (HOST, PORT_VOICE) # Bindings for voice socfet
+ADDR_VIDEO = (HOST, PORT_VIDEO) # Bindings for text socket
 
 
 """
@@ -119,22 +163,32 @@ SERVER_VOICE = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER_VOICE.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 SERVER_VOICE.bind(ADDR_VOICE)
 
+SERVER_VIDEO = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_VIDEO.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SERVER_VIDEO.bind(ADDR_VIDEO)
+
 if __name__ == "__main__":
-	SERVER_TEXT.listen(5)
-	SERVER_VOICE.listen(5)
-	print("Waiting for connection...")
+        SERVER_TEXT.listen(5)
+        SERVER_VOICE.listen(5)
+        SERVER_VIDEO.listen(5)
+        print("Waiting for connection...")
 
-	#Start accepting connection to Text Port
-	ACCEPT_THREAD_TEXT = Thread(target=accept_text)
-	ACCEPT_THREAD_TEXT.start()
+        #Start accepting connection to Text Port
+        ACCEPT_THREAD_TEXT = Thread(target=accept_text).start()
 
-	#Start accepting connection to Voice Port
-	ACCEPT_THREAD_VOICE = Thread(target=accept_voice)
-	ACCEPT_THREAD_VOICE.start()
-    #Join the voice and text threads with the main thread
-    #allow main thread to stop exectuing until execution of voice and text threads are coomplete
-	ACCEPT_THREAD_VOICE.join()
-	ACCEPT_THREAD_TEXT.join()
-    #Close the server sockets
-	SERVER_TEXT.close()
-	SERVER_VOICE.close()
+        #Start accepting connection to Voice Port
+        ACCEPT_THREAD_VOICE = Thread(target=accept_voice).start()
+
+        #Start accepting connection to Video Port
+        ACCEPT_THREAD_VIDEO = Thread(target=accept_video()).start()
+
+        #Join the voice and text threads with the main thread
+        #allow main thread to stop executing until execution of voice,video and text threads are coomplete
+        ACCEPT_THREAD_VOICE.join()
+        ACCEPT_THREAD_TEXT.join()
+        ACCEPT_THREAD_VIDEO.join()
+
+        #Close the server sockets
+        SERVER_TEXT.close()
+        SERVER_VOICE.close()
+        SERVER_VIDEO.close()
